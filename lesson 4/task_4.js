28c7fde9c9ab4ea2c5ab206fdf1fd90545a62bb6
Asror <asrorjonraximovv0099@gmail.com>
2020-12-16 22:15:47 +0500

    add

--- lesson 4/task_4.js
+++ lesson 4/task_4.js
@@ -1,12 +1,14 @@
-let unli = ["a", "e", "i", "o", "u"];
-let satr = "ozbekistonii"
+let unli = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+let satr = "Salom Dunyo"
 let unliSum = 0;
 const strArr = satr.split('')
+console.log("ixtiyoriy so'z: " + satr)
 for (let i of unli) {
     for (let j of strArr){
         if (i === j) {
             unliSum += 1;
+            console.log("unli harf: " + j)
         }
     }
 }
-console.log(unliSum);
+console.log("unli harflarning umumiy soni; " + unliSum);
